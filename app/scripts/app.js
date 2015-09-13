de204ef3d8e98eb76e0e73ad6e3131b8d43f0cba
jftdev <julien.folliot@jft-dev.fr>
2015-09-13 11:49:33 +0200

    Add book details images and contact form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
 
-    console.log('Our app is ready to rock!');
     var title = document.querySelector('.app-name');
     var toolbar = document.querySelector('paper-toolbar');
     var items = document.querySelectorAll('.menu-item');
@@ -44,9 +43,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         item.querySelector('paper-button').style['border-color'] = item.className.indexOf('active') > -1 ? 'rgb(' + red + ',' + green + ',' + blue + ')' : 'transparent';
       });
 
-      //mavhin = 0 alors coleur = 255
-      //machin = 1 alors couleur = 9
-
       title.style.opacity = factor;
       title.style.color = 'rgb(' + red + ',' + green + ',' + blue + ')';
 
@@ -67,6 +63,12 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       });
     });
 
+    document.querySelector('#contact-form .pink-button').addEventListener('click', function() {
+      var form = document.querySelector('#contact-form');
+      /*console.log(form.serialize());*/
+      form.submit();
+    });
+
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
@@ -75,20 +77,20 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   });
 
   app.model = {
-    'PocTan': {
-      id: 'PocTan',
+    'POC-TAN': {
+      id: 'POC-TAN',
       name: 'TAN',
       type: 'POC',
       details: 'Ergonomie / Direction Artistique / Webdesign',
       image: 'images/home/TAN.png',
       description: 'Blablabla très long...',
       images: [
-        "images/book/VISUELS_POC_TAN/01.png",
-        "images/book/VISUELS_POC_TAN/02.png",
-        "images/book/VISUELS_POC_TAN/03.png",
-        "images/book/VISUELS_POC_TAN/04.png",
-        "images/book/VISUELS_POC_TAN/05.png",
-        "images/book/VISUELS_POC_TAN/06.png"
+        'images/book/VISUELS_POC_TAN/01.png',
+        'images/book/VISUELS_POC_TAN/02.png',
+        'images/book/VISUELS_POC_TAN/03.png',
+        'images/book/VISUELS_POC_TAN/04.png',
+        'images/book/VISUELS_POC_TAN/05.png',
+        'images/book/VISUELS_POC_TAN/06.png'
       ]
     },
     'ISDEM': {
@@ -99,12 +101,205 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       image: 'images/home/ISDEM.png',
       description: 'Blablabla très long...',
       images: [
-        "images/book/VISUELS_POC_TAN/01.png",
-        "images/book/VISUELS_POC_TAN/02.png"
+        'images/book/VISUELS_ISDEM/01.png',
+        'images/book/VISUELS_ISDEM/02.png',
+        'images/book/VISUELS_ISDEM/03.png'
+      ]
+    },
+    'BPATL': {
+      id: 'BPATL',
+      name: 'BP-ATL',
+      type: 'Projet',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/BPATL.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_BPATL/01.png',
+        'images/book/VISUELS_BPATL/02.png'
+      ]
+    },
+    'I-BP': {
+      id: 'I-BP',
+      name: 'i-BP',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/I_BP.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_I_BP/01.png',
+        'images/book/VISUELS_I_BP/02.png',
+        'images/book/VISUELS_I_BP/03.png'
+      ]
+    },
+    'GUIMIER': {
+      id: 'GUIMIER',
+      name: 'GUIMIER',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/GUIMIER.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_GUIMIER/01.png',
+        'images/book/VISUELS_GUIMIER/02.png',
+        'images/book/VISUELS_GUIMIER/03.png',
+        'images/book/VISUELS_GUIMIER/04.png'
+      ]
+    },
+    'DOCKER': {
+      id: 'DOCKER',
+      name: 'DOCKER',
+      type: 'Cheat Sheet',
+      details: 'Direction Artistique',
+      image: 'images/home/DOCKER.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_BREIZHCAMP/01.png',
+        'images/book/VISUELS_BREIZHCAMP/02.png',
+        'images/book/VISUELS_BREIZHCAMP/03.png',
+        'images/book/VISUELS_BREIZHCAMP/04.png',
+        'images/book/VISUELS_BREIZHCAMP/05.png',
+        'images/book/VISUELS_BREIZHCAMP/06.png'
+      ]
+    },
+    'MPA': {
+      id: 'MPA',
+      name: 'MPA',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/MPA.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_MPA/01.png',
+        'images/book/VISUELS_MPA/02.png',
+        'images/book/VISUELS_MPA/03.png'
+      ]
+    },
+    '24H': {
+      id: '24H',
+      name: '24H DU MANS',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/24H.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_ACO/01.png',
+        'images/book/VISUELS_ACO/02.png',
+        'images/book/VISUELS_ACO/03.png',
+        'images/book/VISUELS_ACO/04.png'
+      ]
+    },
+    'NESTLE-WATERS': {
+      id: 'NESTLE-WATERS',
+      name: 'NESTLE WATERS',
+      type: 'Projet',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/NESTLE_WATERS.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_NESTLE_WATERS/01.png',
+        'images/book/VISUELS_NESTLE_WATERS/02.png',
+        'images/book/VISUELS_NESTLE_WATERS/03.png'
+      ]
+    },
+    'PENIBILITE': {
+      id: 'PENIBILITE',
+      name: 'Pénibilité',
+      type: 'POC',
+      details: 'Ergonomie / Direction Artistique / Webdesign',
+      image: 'images/home/POC_PENIBILITE.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_POC_PENIBILITE/01.png'
+      ]
+    },
+    'IMMO': {
+      id: 'IMMO',
+      name: 'Agence Immobilière',
+      type: 'POC',
+      details: 'Ergonomie / Direction Artistique / Webdesign',
+      image: 'images/home/POC_IMMO.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_POC_IMMO/01.png',
+        'images/book/VISUELS_POC_IMMO/02.png'
+      ]
+    },
+    'CBP': {
+      id: 'CBP',
+      name: 'CBP',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/CBP.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_CBP/01.png',
+        'images/book/VISUELS_CBP/02.png'
+      ]
+    },
+    'MEETUP': {
+      id: 'MEETUP',
+      name: 'MEETUP',
+      type: 'Projet interne',
+      details: 'Direction Artistique',
+      image: 'images/home/MEETUP.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_MEETUP/01.png',
+        'images/book/VISUELS_MEETUP/02.png',
+        'images/book/VISUELS_MEETUP/03.png',
+        'images/book/VISUELS_MEETUP/04.png'
+      ]
+    },
+    'ENS-RENNES': {
+      id: 'ENS-RENNES',
+      name: 'ENS Rennes',
+      type: 'Avant-Vente',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/ENS_RENNES.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_ENS_RENNES/01.png',
+        'images/book/VISUELS_ENS_RENNES/02.png'
+      ]
+    },
+    'FONTEVRAUD': {
+      id: 'FONTEVRAUD',
+      name: 'FONTEVRAUD',
+      type: 'Projet',
+      details: 'Webdesign',
+      image: 'images/home/FONTEVRAUD.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_FONTEVRAUD/01.png',
+        'images/book/VISUELS_FONTEVRAUD/02.png',
+        'images/book/VISUELS_FONTEVRAUD/03.png',
+        'images/book/VISUELS_FONTEVRAUD/04.png'
+      ]
+    },
+    'FRANSBONHOMME': {
+      id: 'FRANSBONHOMME',
+      name: 'FRANSBONHOMME',
+      type: 'Projet',
+      details: 'Direction Artistique / Webdesign',
+      image: 'images/home/FRANSBONHOMME.png',
+      description: 'Blablabla très long...',
+      images: [
+        'images/book/VISUELS_FRANSBONHOMME/01.png',
+        'images/book/VISUELS_FRANSBONHOMME/02.png'
       ]
     }
   };
 
-  app.currentModel;
+  /*
+
+   <form action="//flipmail.co/api/7kc4YcCwFT0TFC01bBRd" method="post">
+   <input type="text" name="name">
+   <input type="email" name="email">
+   <textarea name="message"></textarea>
+   <button type="submit">Submit Form</button>
+   </form>
+
+
+   */
 
 })(document);
